cb38e97b9afe13219a6b0db97e6bbf4d7cfe6f82
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-05-28 14:19:38 +0300

    scrollview implemented for recipedetail screen

--- lib/recipedetailscreen.dart
+++ lib/recipedetailscreen.dart
@@ -1,6 +1,6 @@
-import "package:flutter/material.dart";
-import "package:school_project/models/recipes_model.dart";
-import "package:school_project/widgets/ingredientscard.dart";
+import 'package:flutter/material.dart';
+import 'package:school_project/models/recipes_model.dart';
+import 'package:school_project/widgets/ingredientscard.dart';
 
 class RecipeDetail extends StatelessWidget {
   final Recipes recipe;
@@ -10,16 +10,17 @@ class RecipeDetail extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        home: Scaffold(
-      appBar: AppBar(
+      home: Scaffold(
+        appBar: AppBar(
           shadowColor: Colors.black.withOpacity(1),
           backgroundColor: Colors.orange,
           title: const Text(
             "Recipe Detail",
             style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold),
+              color: Colors.white,
+              fontSize: 24.0,
+              fontWeight: FontWeight.bold,
+            ),
           ),
           centerTitle: true,
           leading: IconButton(
@@ -27,61 +28,59 @@ class RecipeDetail extends StatelessWidget {
             onPressed: () {
               Navigator.pop(context);
             },
-          )),
-      body: Column(
-        children: [
-          // Add this line
-          Container(
-              height: 300,
-              width: double.maxFinite,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: NetworkImage(recipe.image),
-                  fit: BoxFit.cover,
-                ),
-              )),
-          ListTile(
-            title: Text(
-              recipe.title,
-              style: const TextStyle(
-                fontSize: 32,
-                fontWeight: FontWeight.bold,
-                fontStyle: FontStyle.italic,
-                color: Colors.orange,
-              ),
-            ),
-            subtitle: Text(recipe.description,
-                style: const TextStyle(
-                  fontSize: 16,
-                  fontWeight: FontWeight.normal,
-                  fontStyle: FontStyle.normal,
-                  color: Colors.black,
-                )),
           ),
-
-          Expanded(
-            child: ListView.builder(
-              itemCount: recipe.ingredients.length,
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 16.0, vertical: 0.0),
-                  // child: Text(
-                  //   recipe.ingredients[index],
-                  //   style: const TextStyle(
-                  //     fontSize: 16,
-                  //     color: Colors.black,
-                  //   ),
-                  // ),
-                  child: IngredientsCard(
-                    ingredient: recipe.ingredients[index],
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Container(
+                height: 300,
+                width: double.maxFinite,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: NetworkImage(recipe.image),
+                    fit: BoxFit.cover,
                   ),
-                );
-              },
-            ),
+                ),
+              ),
+              ListTile(
+                title: Text(
+                  recipe.title,
+                  style: const TextStyle(
+                    fontSize: 32,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.italic,
+                    color: Colors.orange,
+                  ),
+                ),
+                subtitle: Text(
+                  recipe.description,
+                  style: const TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.normal,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+              ListView.builder(
+                shrinkWrap: true,
+                physics: NeverScrollableScrollPhysics(),
+                itemCount: recipe.ingredients.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 16.0, vertical: 0.0),
+                    child: IngredientsCard(
+                      ingredient: recipe.ingredients[index],
+                    ),
+                  );
+                },
+              ),
+            ],
           ),
-        ],
+        ),
       ),
-    ));
+    );
   }
 }
