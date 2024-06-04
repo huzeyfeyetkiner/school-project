1784950f47103829e76dd0ae44ba9bc8394978b4
Huzeyfe Yetkiner <yetkinerhuzeyfe@gmail.com>
2024-06-04 18:23:44 +0300

    profile screen implementation

--- lib/profilescreen.dart
+++ lib/profilescreen.dart
@@ -6,42 +6,253 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          shadowColor: Colors.black.withOpacity(1),
-          backgroundColor: Colors.orange,
-          title: const Text(
-            "Profile",
-            style: TextStyle(
-                color: Colors.white,
-                fontSize: 24.0,
-                fontWeight: FontWeight.bold),
-          ),
-          centerTitle: true,
-          actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/recipes');
-              },
-              child: const Icon(
-                Icons.restaurant,
-                color: Colors.white,
-              ),
-            ),
-          ],
+        home: Scaffold(
+      appBar: AppBar(
+        shadowColor: Colors.black.withOpacity(1),
+        backgroundColor: Colors.orange,
+        title: const Text(
+          "Profile",
+          style: TextStyle(
+              color: Colors.white, fontSize: 24.0, fontWeight: FontWeight.bold),
         ),
-        body: const Column(
-          children: [
+        centerTitle: true,
+        actions: [
+          TextButton(
+            onPressed: () {
+              Navigator.pushNamed(context, '/recipes');
+            },
+            child: const Icon(
+              Icons.restaurant,
+              color: Colors.white,
+            ),
+          ),
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(8, 20, 8, 20),
+        child: Column(children: [
+          const Row(
+            children: [
+              CircleAvatar(
+                radius: 50.0,
+                backgroundImage: NetworkImage(
+                    "https://thefoxwp.com/wp-content/uploads/2015/02/member131-570x570.jpg"),
+              ),
+              SizedBox(width: 20.0),
+              Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+                Text(
+                  "Aykut Kocaman",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 16.0,
+                      fontWeight: FontWeight.bold),
+                ),
+                Text(
+                  "01/01/1988",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 14.0,
+                      fontWeight: FontWeight.w400),
+                ),
+              ]),
+            ],
+          ),
+          const SizedBox(height: 10.0),
+          const Row(children: [
+            Icon(Icons.email, color: Colors.orange),
+            SizedBox(width: 10.0),
             Text(
-              "Profile",
+              "Email Address:",
               style: TextStyle(
                   color: Colors.black,
-                  fontSize: 24.0,
+                  fontSize: 14.0,
                   fontWeight: FontWeight.bold),
             ),
-          ],
-        ),
+            SizedBox(width: 10.0),
+            Text("aykutkocaman1@gmail.com")
+          ]),
+          const SizedBox(height: 10.0),
+          const Row(children: [
+            Icon(Icons.phone, color: Colors.orange),
+            SizedBox(width: 10.0),
+            Text(
+              "Phone:",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 10.0),
+            Text("+90 555 555 55 55")
+          ]),
+          const SizedBox(height: 10.0),
+          const Row(children: [
+            Icon(Icons.location_city, color: Colors.orange),
+            SizedBox(width: 10.0),
+            Text(
+              "Address:",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 10.0),
+            Text("Istanbul, Turkey")
+          ]),
+          const SizedBox(height: 10.0),
+          const Row(children: [
+            Icon(Icons.work, color: Colors.orange),
+            SizedBox(width: 10.0),
+            Text(
+              "Job:",
+              style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(width: 10.0),
+            Text("Software Developer")
+          ]),
+          const SizedBox(height: 10.0),
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(children: [
+                  Icon(
+                    Icons.favorite,
+                    color: Colors.orange,
+                    size: 30,
+                  ),
+                  SizedBox(width: 10.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Favorite Recipes",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "12",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  )
+                ]),
+                Row(children: [
+                  Icon(
+                    Icons.food_bank,
+                    color: Colors.orange,
+                    size: 30,
+                  ),
+                  SizedBox(width: 10.0),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Maden Recipes",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "3",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 16.0,
+                            fontWeight: FontWeight.w400),
+                      ),
+                    ],
+                  )
+                ]),
+              ],
+            ),
+          ),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/editprofile');
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text(
+                "Edit Profile",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/editprofile');
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text(
+                "Application Settings",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/editprofile');
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text(
+                "Contact Us",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          SizedBox(
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/editprofile');
+              },
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+              child: const Text(
+                "FAQ",
+                style:
+                    TextStyle(color: Colors.black, fontWeight: FontWeight.bold),
+              ),
+            ),
+          )
+        ]),
       ),
-    );
+    ));
   }
 }
